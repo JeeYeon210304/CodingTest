6323ae1012c0b326c00233dca59f9b4202ab581b
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-04-18 14:26:34 +0900

    CondingTest_1_3_cat_succeed
    baekJun CondingTest

---
+++ CondingTest_1/src/com/test03/Main.java
@@ -0,0 +1,10 @@
+package com.test03;
+
+public class Main {
+	 public static void main(String[] args){
+		 System.out.println("\\    /\\");
+		 System.out.println(" )  ( ')");
+		 System.out.println("(  /  )");
+		 System.out.println(" \\(__)|");
+	}
+}
