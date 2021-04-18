4b285a59136977bd8130dbde235ddac126172ee2
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-04-18 14:43:54 +0900

    CondingTest_1_4_dog_succeed

--- CondingTest_1/src/com/test04/Main.java
+++ CondingTest_1/src/com/test04/Main.java
@@ -3,10 +3,10 @@ package com.test04;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("|\\_//|");
-		System.out.println("|q p|   //}");
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
 		System.out.println("( 0 )\"\"\"\\");
 		System.out.println("|\"^\"`    |");
-		System.out.println("||_/=\\__|");
+		System.out.println("||_/=\\\\__|");
 	}
 }
