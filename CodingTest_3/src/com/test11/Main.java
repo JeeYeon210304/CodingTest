46a8f73ec9fcb0d884938744448a531eeaf1faec
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-04-29 22:42:20 +0900

    CodingTest_3-11_X보다 작은 수_succeed

--- CodingTest_3/src/com/test11/Main.java
+++ CodingTest_3/src/com/test11/Main.java
@@ -1,10 +1,25 @@
 package com.test11;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int X = sc.nextInt();
+		int Arr[] = new int[N];
 		
+		for (int i = 0; i < N; i++) {
+			Arr[i] = sc.nextInt();
+		}	
 		
+		for(int i = 0; i < N;  i++) {
+			if (Arr[i] < X) {
+				System.out.print(Arr[i] + " ");
+			}
+		}
+		sc.close();
 	}
 }
