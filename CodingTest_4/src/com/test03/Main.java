27a69331a825700138661c008a6f3dfc4c4f6a20
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-05-04 21:21:54 +0900

    CodingTest_43-_더하기 사이클_succeed

---
+++ CodingTest_4/src/com/test03/Main.java
@@ -0,0 +1,28 @@
+package com.test03;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		sc.close();
+		int cnt = 0;
+		int end = n;
+		
+		while (true) {
+			
+			n = ((n % 10) * 10) + (((n / 10) + (n % 10)) % 10);
+			cnt++;
+			
+			if (end == n) {
+				break;
+			}
+		}
+		System.out.println(cnt);
+		
+	}
+}
