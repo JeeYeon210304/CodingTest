9b83306522b6fa93b3cae6a99fd3b0d26500450f
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-05-11 21:45:21 +0900

    CodingTest_5-2_최댓값_succeed

--- CodingTest_05/src/com/test02/Main.java
+++ CodingTest_05/src/com/test02/Main.java
@@ -1,5 +1,32 @@
 package com.test02;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[9];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		int max = 0;
+		int count = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] > max) {
+				max = arr[i];
+				count = i + 1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(count);
+	}
 }
+
+	
+	
+	
