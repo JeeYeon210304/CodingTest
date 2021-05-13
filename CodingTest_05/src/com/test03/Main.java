328c81c402c5556b414512e9d3438183b032a02f
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-05-13 21:38:46 +0900

    CodingTest_5-3_숫자의 개수_succeed

--- CodingTest_05/src/com/test03/Main.java
+++ CodingTest_05/src/com/test03/Main.java
@@ -1,5 +1,24 @@
 package com.test03;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int value = (sc.nextInt() * sc.nextInt() * sc.nextInt());
+		sc.close();
+		
+		String input  = String.valueOf(value);
+		char[] ch = input.toCharArray();
+		
+		int[] count = new int[10];
+		
+		for(int i=0; i < ch.length; i++) {
+			count[ch[i] - '0']++;
+		}
+		for(int res: count) {
+			System.out.println(res);
+		}
+	}
 }
