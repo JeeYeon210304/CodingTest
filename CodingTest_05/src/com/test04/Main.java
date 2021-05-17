edd8f677a0df55a26006daf4f5082e2b4e8f45bc
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-05-17 21:44:04 +0900

    CodingTest_5-4_나머지_succeed

--- CodingTest_05/src/com/test04/Main.java
+++ CodingTest_05/src/com/test04/Main.java
@@ -1,5 +1,37 @@
 package com.test04;
 
+import java.util.Scanner;
+
 public class Main {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int[] a = new int[10];
+		
+		for(int i = 0; i < 10; i++) {
+			a [i] = sc.nextInt();
+		}
+		sc.close();
+		
+		for(int i = 0; i < 10; i++) {
+			a [i] = a[i] % 42;
+		}
+		
+		int result = 0;
+		
+		for(int i = 0; i < 10; i++) {
+			int count = 0;
+			for(int j = i+1; j < 10; j++) {
+				if (a[i] == a[j]) {
+					count++;
+				}
+			}
+			if (count == 0) {
+				result++;
+			}
+		}
+		System.out.println(result);
+	}
 }
