6b175a98e9f7d3c0f75d668244155d5d05ddc63a
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-05-20 22:05:15 +0900

    CodingTest_5-5_평균_succeed

--- CodingTest_05/src/com/test05/Main.java
+++ CodingTest_05/src/com/test05/Main.java
@@ -1,5 +1,34 @@
 package com.test05;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double[] a = new double[sc.nextInt()];
+		
+		for(int i = 0; i < a.length; i++) {
+			a[i] = sc.nextDouble();
+		}
+		sc.close();
+		
+		double res = 0;
+		Arrays.sort(a);
+		
+		for(int i = 0; i < a.length; i++) {
+			res += ((a[i]/a[a.length-1])*100);
+		}
+		System.out.println(res/a.length);
+	}
 }
+
+
+
+
+
+
+
