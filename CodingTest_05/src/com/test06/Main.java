3c11f506997ce89654a39a6a8bd3cbbff3c973ed
Yeon <80025908+JeeYeon210304@users.noreply.github.com>
2021-05-25 23:02:47 +0900

    CodingTest_5-6_OX퀴즈_succeed

--- CodingTest_05/src/com/test06/Main.java
+++ CodingTest_05/src/com/test06/Main.java
@@ -1,5 +1,34 @@
 package com.test06;
 
+import java.util.Scanner;
+
 public class Main {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String[] array = new String[sc.nextInt()];
+
+		for (int i = 0; i < array.length; i++) {
+			array[i] = sc.next();
+		}
+		sc.close();
+
+		for (int i = 0; i < array.length; i++) {
+			int count = 0;
+			int sum = 0;
+
+			for (int j = 0; j < array[i].length(); j++) {
+
+				if (array[i].charAt(j) == 'O') {
+					count++;
+				} else {
+					count = 0;
+				}
+				sum += count;
+			}
+			System.out.println(sum);
+		}
+	}
 }
